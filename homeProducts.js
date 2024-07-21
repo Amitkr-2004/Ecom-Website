5e27c812b600c0e866c7428868a82e41aae3552f
Amit Kumar <amitkumar990374@gmail.com>
2024-07-21 14:25:21 +0530

    toggling effext on home screen

--- homeProducts.js
+++ homeProducts.js
@@ -1,3 +1,4 @@
+import { homeQuantityToggle } from "./homeQuantityToggle";
 const ProductContainer =document.querySelector('#productContainer')
 const ProductTemplate =document.querySelector('#productTemplate')
 
@@ -7,13 +8,14 @@ export const showProductContainer =(products) =>{
     }
 
     products.forEach((currProduct)=>{
-        const {id,name,category,brand,price,stock,description,image} = currProduct;
+        const {id,name,category,price,stock,description,image} = currProduct;
 
         const productTemplateClone = document.importNode(ProductTemplate.content,true)     //this will make a clone of the ProductTemplate and true is used so that it can make a copy of descendents of ProductTemplate
 
         //during filling it no need to maintain the same order of filling as we have cloned our template thus filling of data will be in same order
 
-        
+        //now for identity we need id
+        productTemplateClone.querySelector("#cardsValue").setAttribute('id',`card${id}`)     //for randomly setting the id value inplace of id   
 
         productTemplateClone.querySelector(".category").textContent = category;  //if we want to clone any text then we have to use textContent
         productTemplateClone.querySelector(".productName").textContent=name;
@@ -23,6 +25,12 @@ export const showProductContainer =(products) =>{
         productTemplateClone.querySelector(".productPrice").textContent = `₹${price}`;
         productTemplateClone.querySelector(".productActualPrice").textContent = `₹${4*price}`;
         productTemplateClone.querySelector(".productStock").textContent=stock;
+
+        //now for toggling the value of cardIncrement and cardDecrement 
+        productTemplateClone.querySelector('.stockElement').addEventListener("click",(event)=>{
+            homeQuantityToggle(event,id,stock)
+        }
+        )
 
         ProductContainer.append(productTemplateClone)
     })
---
+++ homeQuantityToggle.js
@@ -0,0 +1,28 @@
+export const homeQuantityToggle = (event,id,stock) =>{
+    const productID=document.querySelector(`#card${id}`);   //as the id name is changed to card1,card2.... in homeProduct.js
+    // console.log(product);
+
+    const productQuantity = productID.querySelector(".productQuantity");
+    // console.log(productQuantity);
+
+    let quantity = parseInt(productQuantity.getAttribute('data-quantity')) || 1;    //getting the attribute which is set before else initially 1
+
+    if(event.target.className === 'cartIncrement'){
+        if(quantity<stock){
+            quantity+=1;
+        }
+        else if(quantity==stock){
+            quantity=stock;
+        }
+    }
+
+    if(event.target.className === 'cartDecrement'){
+        if(quantity>1){
+            quantity-=1;
+        }
+    }
+
+    productQuantity.innerHTML=quantity;
+    productQuantity.setAttribute('data-quantity',quantity);         //setting the attribute data-quantity to new quantity after increment or decrement
+    return quantity;
+}
