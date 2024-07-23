a0923724dab93e5aadb7c867ff9c11a012c270f4
Amit Kumar <amitkumar990374@gmail.com>
2024-07-23 15:10:50 +0530

    Add to Cart-2

--- addToCart.js
+++ addToCart.js
@@ -1,19 +1,43 @@
 import { getCartProductsFromLS } from "./getCartProductsFromLS";
+import { homeQuantityToggle } from "./homeQuantityToggle";
 import { UpdateCartValue } from "./UpdateCartValue";
+
+getCartProductsFromLS();
 
 export const addToCart = (event,id,stock) =>{
     let arrLocalStorageProduct = getCartProductsFromLS();   //this function basically retrieves all the data stored in LocalStorage 
 
-    let productID = document.querySelector(`#card${id}`);
-    console.log(productID);
+    let product = document.querySelector(`#card${id}`);
+    console.log(product);
 
-    let productQuantity= productID.querySelector('.productQuantity').innerHTML;     //as we need only the value not complete paragraph
+    let productQuantity= product.querySelector('.productQuantity').innerHTML;     //as we need only the value not complete paragraph
 
-    let productPrice= productID.querySelector('.productPrice').innerHTML;           //as we need only the value not complete paragraph
+    let productPrice= product.querySelector('.productPrice').innerHTML;           //as we need only the value not complete paragraph
 
     // console.log(productQuantity,productPrice);
 
     productPrice=productPrice.replace('₹','')   //as productPrice contains both a character and integer thus remove symbol
+
+    let existingProd = arrLocalStorageProduct.find((product)=>product.id===id)  //if the product is already present in arrLocalStorageProduct then just return false and as it is an object therefore we need to write a function
+
+    // console.log(existingProd.productQuantity)
+
+    if(existingProd && productQuantity>1){      //this is used for updating the quantity And its price when user has increase the cart value
+        productQuantity=Number(productQuantity)+Number(existingProd.productQuantity)    
+        productPrice=Number(productPrice*productQuantity);  //converting to number
+        
+        let updatedCart={id,productPrice,productQuantity}       //now all the new values are added to the updatedCart
+        updatedCart = arrLocalStorageProduct.map((currProduct)=>{   //now the new values are added to the array whose id got matched else remains the same
+            if(currProduct.id==id)  return updatedCart
+            else    return currProduct;
+        })
+        localStorage.setItem('cartProductsLS',JSON.stringify(updatedCart));
+    }
+    if(existingProd){
+        // alert('already Present')
+        return false;
+    }
+
     productPrice=Number(productPrice*productQuantity);  //converting to number 
     productQuantity=Number(productQuantity)     //converting to number
 
@@ -21,6 +45,6 @@ export const addToCart = (event,id,stock) =>{
     localStorage.setItem('cartProductsLS',JSON.stringify(arrLocalStorageProduct));  //finally converting object to JSON format and inserting in LS
     // console.log(arrLocalStorageProduct.length)
 
-    //updating cart buttion value
+    //updating cart button value
     UpdateCartValue(arrLocalStorageProduct)
 }
--- getCartProductsFromLS.js
+++ getCartProductsFromLS.js
@@ -1,9 +1,16 @@
+import { UpdateCartValue } from "./UpdateCartValue";
+
 export const getCartProductsFromLS = () =>{
     let CartProducts=localStorage.getItem("cartProductsLS");     //through this all the item from local storage we will get having key value cartProductLS
 
     if(!CartProducts){      //if cartProductsLS is empty then return empty array
         return [];
     }
+
     CartProducts=JSON.parse(CartProducts)   //else the CartProducts values which is in JSON format will be parsed to object
+
+    //updating cart button value
+    UpdateCartValue(CartProducts);
+
     return CartProducts;
 }
